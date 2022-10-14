710683a8d4fbf114e09a374c4a11ef392f60c123
takenokoroid <takenokoroid@gmail.com>
2022-10-14 20:07:55 +0900

    ワイヤーフレームの作成とブラウザのURLを取ってこれるようにした

---
+++ src/lib/Top.svelte
@@ -0,0 +1,25 @@
+<script lang="ts">
+  let url = "";
+  const cutting = () => {
+    console.log("cut");
+  };
+  const pickup = () => {
+    console.log("pickup");
+    chrome.tabs.query({ active: true, lastFocusedWindow: true }, (tabs) => {
+      url = tabs[0].url;
+    });
+  };
+</script>
+
+<div>
+  <h1>ピカピカ</h1>
+  <button type="button" on:click={cutting}>cutting, URL</button>
+  <button type="button" on:click={pickup}>pick up, URL</button>
+  {url}
+</div>
+
+<style>
+  div {
+    width: 500px;
+  }
+</style>
